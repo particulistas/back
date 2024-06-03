7c4b1c92624361fac7745888b8a1f70fed36d992
xpestana <unibasileaweb@gmail.com>
2024-06-03 15:15:51 -0400

    categories

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Category;
+
+class CategorySeeder extends Seeder
+{
+    public function run()
+    {
+        $categories = [
+            'Local y nave' => ['local', 'nave'],
+            'Habitaciones' => ['Habitaciones'],
+            'Garage' => ['Garage'],
+            'Oficinas' => ['Oficinas'],
+            'Trastero' => ['Trastero'],
+            'Vivienda' => ['Piso', 'Ático', 'Casa', 'Chalet', 'Duplex', 'Loft', 'Apartamento', 'Estudio'],
+            'Casa y Chalet' => ['Piso', 'Ático', 'Casa', 'Chalet', 'Duplex', 'Loft', 'Apartamento', 'Estudio'],
+        ];
+
+        foreach ($categories as $parent => $children) {
+            $parentCategory = Category::create(['name' => $parent, 'parent_id' => null]);
+
+            foreach ($children as $child) {
+                Category::create(['name' => $child, 'parent_id' => $parentCategory->id]);
+            }
+        }
+    }
+}
+
