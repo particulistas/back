0b8aab2eca66e39374873a40a43fd43dfa6b756a
xpestana <unibasileaweb@gmail.com>
2024-09-10 09:23:14 -0400

    aasd

--- database/migrations/2024_08_22_153604_create_properties_table.php
+++ database/migrations/2024_08_22_153604_create_properties_table.php
@@ -13,13 +13,39 @@ return new class extends Migration
     {
         Schema::create('properties', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('category_id')->constrained()->onDelete('cascade');
-            $table->enum('transaction', ['sale', 'rental', 'both']);
-            $table->float('sale_price', precision: 53, scale: 4);
-            $table->float('rental_price', precision: 53, scale: 4);
-            $table->string('m_built');
-            $table->string('m_usefull');
-            $table->integer('bathrooms');
+            $table->foreignId('category_id')->constrained()->onDelete('cascade'); //debe ir la subcategoria
+            $table->enum('transaction', ['sale', 'rental', 'both'])->default('sale');
+            $table->float('sale_price', precision: 53, scale: 4)->nullable();
+            $table->float('rental_price', precision: 53, scale: 4)->nullable();
+            $table->json('bills')->nullable();
+            $table->string('m_built')->nullable();
+            $table->string('m_usefull')->nullable();
+            $table->integer('bathrooms')->nullable();//numero de baños
+            $table->integer('number_plants')->nullable();//numero de plantas
+            $table->integer('number_habs')->nullable();//numero de plantas
+            $table->string('distibutions')->nullable();
+            $table->string('state')->nullable(); //estado del inmueble
+            $table->string('equipment')->nullable();
+            $table->json('ubication')->nullable();
+            $table->json('characteristics')->nullable();
+            $table->json('preferences')->nullable();
+            $table->json('cohabitation')->nullable();
+            $table->integer('antique')->nullable();
+            $table->string('address')->nullable();
+            $table->string('latitude')->nullable();
+            $table->string('longitude')->nullable();
+            $table->boolean('hide_address')->default(0);
+            $table->boolean('top_floor')->default(0);
+            $table->string('door')->nullable();
+            $table->text('description')->nullable();
+            $table->text('optionals')->nullable();
+            $table->enum('energy_certificate', ['yes', 'process', 'exempt'])->default('exempt');
+            $table->json('energy_certificate_yes')->nullable();
+            $table->boolean('publish_phone')->default(0);
+            $table->string('phone')->nullable();
+            $table->enum('phone_characteristics', ['calls', 'whatsapp', 'both'])->default('both');
+            $table->enum('status', ['Daft', 'Publish'])->default('Draft');
+            $table->text('optionals')->nullable(); //caracteristicas opcionales de la vivienda (habitaciones)
             $table->timestamps();
         });
     }
