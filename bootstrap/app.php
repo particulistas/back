a70a749f6e074647ee07c78acc6ed0271a95c8d2
xpestana <unibasileaweb@gmail.com>
2024-09-12 10:46:20 -0400

    delete web and api

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,8 +10,6 @@ use Illuminate\Auth\AuthenticationException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        //web: __DIR__.'/../routes/web.php',
-        //api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
         using: function () {
