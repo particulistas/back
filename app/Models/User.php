e22786516a52e197b638d83c34c86ebf09ddcc6c
xpestana <unibasileaweb@gmail.com>
2025-03-07 10:31:33 -0400

    Fix casing of Profile model in User model's profile method

--- app/Models/User.php
+++ app/Models/User.php
@@ -57,6 +57,6 @@ class User extends Authenticatable
     }
     public function profile()
     {
-        return $this->hasOne(profile::class);
+        return $this->hasOne(Profile::class);
     }
 }
